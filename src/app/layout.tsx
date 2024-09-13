c4a06abfbee62b444e43130d3f2d606e86f75714
Interlinker0115 <interlinker422031@gmail.com>
2024-09-13 03:03:25 -0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,10 @@ import dynamic from "next/dynamic";
 const JotaiProvider = dynamic(() => import("@/providers/jotaiProvider"), { ssr: false });
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '400', '700', '900'], // Changed from "weights" to "weight"
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,8 +20,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;400;700;900&display=swap" rel="stylesheet" />
         <JotaiProvider>
           {children}
         </JotaiProvider>
