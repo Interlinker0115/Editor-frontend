40d369db5e352c948a70dc6efe3439cdfe90dc32
Interlinker0115 <interlinker422031@gmail.com>
2024-10-29 06:46:01 +0900

    fix the size of terms

--- src/app/component/footer/footer.js
+++ src/app/component/footer/footer.js
@@ -4,10 +4,10 @@ const Footer = () => {
     const router = useRouter();
     return (
         <div className=" w-full flex justify-between bg-[#0D6EFD] px-[20px] md:px-[30px] lg:px-[45px] xl:px-[55px] min-h-[88px] items-center py-[10px] lg:py-0" id="contact">
-            <div className="text-[14px] sm:text-[16px] md:text-[18px] xl:text-[20px] 2xl:text-[20px] font-medium text-white hidden lg:block">© Hispid Consulting LLC, 2024. Created by DocuClever Team</div>
-            <div className="text-[14px] sm:text-[16px] md:text-[18px] xl:text-[20px] 2xl:text-[20px] font-bold text-white hover:cursor-pointer" onClick={() => router.push("/reference/contact")}>Contact</div>
-            <div className="text-[14px] sm:text-[16px] md:text-[18px] xl:text-[20px] 2xl:text-[20px] font-bold text-white hover:cursor-pointer" onClick={() => router.push("/reference/terms")}>Terms of Service</div>
-            <div className="text-[14px] sm:text-[16px] md:text-[18px] xl:text-[20px] 2xl:text-[20px] font-bold text-white hover:cursor-pointer" onClick={() => router.push("/reference/policy")}>Privacy Policy</div>
+            <div className="text-[12px] sm:text-[14px] md:text-[16px] xl:text-[18px] 2xl:text-[20px] font-medium text-white hidden lg:block">© Hispid Consulting LLC, 2024. Created by DocuClever Team</div>
+            <div className="text-[12px] sm:text-[14px] md:text-[16px] xl:text-[18px] 2xl:text-[20px] font-bold text-white hover:cursor-pointer" onClick={() => router.push("/reference/contact")}>Contact</div>
+            <div className="text-[12px] sm:text-[14px] md:text-[16px] xl:text-[18px] 2xl:text-[20px] font-bold text-white hover:cursor-pointer" onClick={() => router.push("/reference/terms")}>Terms of Service</div>
+            <div className="text-[12px] sm:text-[14px] md:text-[16px] xl:text-[18px] 2xl:text-[20px] font-bold text-white hover:cursor-pointer" onClick={() => router.push("/reference/policy")}>Privacy Policy</div>
         </div>
     )
 }
